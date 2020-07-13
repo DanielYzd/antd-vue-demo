a582c5a2e1b96e45b88e091a660129758d9354dc
admin <admin@admindeMacBook-Pro.local>
2020-07-13 19:26:40 +0800

    11

--- src/views/Bread.vue
+++ src/views/Bread.vue
@@ -4,7 +4,11 @@ export default {
   components: {
     ABreadcrumb: Breadcrumb,
     ABreadcrumbItem: Breadcrumb.Item,
-    AIcon: Icon
+    AIcon: Icon,
+    ABreadcrumbSeparator:Breadcrumb.Separator
+  },
+  created(){
+     
   },
   render() {
     return (
@@ -26,6 +30,26 @@ export default {
           </a-breadcrumb-item>
           <a-breadcrumb-item>Application</a-breadcrumb-item>
         </a-breadcrumb>
+        <h3>自定义分隔符</h3>
+        <a-breadcrumb separator=">">
+          <a-breadcrumb-item>11</a-breadcrumb-item>
+          <a-breadcrumb-item>2222</a-breadcrumb-item>
+          <a-breadcrumb-item>333</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-breadcrumb>
+          <span slot="separator">-----</span>
+          <a-breadcrumb-item>11</a-breadcrumb-item>
+          <a-breadcrumb-item>2222</a-breadcrumb-item>
+          <a-breadcrumb-item>333</a-breadcrumb-item>
+        </a-breadcrumb>
+        <h3>使用 Breadcrumb.Separator 可以自定义分隔符。</h3>
+         <a-breadcrumb separator="">
+          <a-breadcrumb-item>11</a-breadcrumb-item>
+          <a-breadcrumb-separator>****</a-breadcrumb-separator>
+          <a-breadcrumb-item>2222</a-breadcrumb-item>
+          <a-breadcrumb-item>333</a-breadcrumb-item>
+        </a-breadcrumb>
+        <h3>和vue-router结合使用</h3>
       </div>
     );
   }
