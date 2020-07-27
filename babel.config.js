319c931ec70a429d347ab445c4559be695c748f9
admin <admin@admindeMacBook-Pro.local>
2020-07-27 09:33:51 +0800

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: ['@vue/cli-plugin-babel/preset'],
   plugins: [
     [
-      "import",
+      'import',
       /**
        * 本项目基于vuecli4，这边配置跟官网有点区别，否则影响组件样式
        */
       {
-        libraryName: "ant-design-vue",
-        libraryDirectory: "es",
-        style: "css"
+        libraryName: 'ant-design-vue',
+        libraryDirectory: 'es',
+        style: 'css'
       }
     ]
   ]
-};
+}
